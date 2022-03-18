bb6acd420c2b730c9f2e6cddf6fd1f69d373b982
Peemapos.At <79019396+Peemapos@users.noreply.github.com>
2022-03-18 15:02:00 +0700

    Add files via upload

---
+++ rent.py
@@ -0,0 +1,59 @@
+import json
+from web3 import Web3
+#import RPi.GPIO as GPIO
+from time import sleep
+
+web_3=Web3(Web3.HTTPProvider("HTTP://127.0.0.1:7545"))
+chainid=1377
+address=input("Input your address: ")
+private_key=input("Input your privatekey:0x")
+host="0xd95eF0F66BA6adAf5c23F467B2fEdc3247d5a77c"
+
+nonce=web_3.eth.getTransactionCount(address)
+day=input("How many day: ")
+if day == "1":
+    pay = {
+        'nonce': nonce,
+        'to': host,
+        'value': web_3.toWei(1, 'ether'),
+        'gas': 2000000,
+        'gasPrice': web_3.toWei('50', 'gwei')
+    }
+
+    #sign the transaction
+    signed_pay = web_3.eth.account.sign_transaction(pay, private_key)
+
+    #send transaction
+    pay_hash = web_3.eth.sendRawTransaction(signed_pay.rawTransaction)
+    print("You rent 1 day")
+
+if day == "3":
+    pay = {
+        'nonce': nonce,
+        'to': host,
+        'value': web_3.toWei(3, 'ether'),
+        'gas': 2000000,
+        'gasPrice': web_3.toWei('50', 'gwei')
+    }
+
+    #sign the transaction
+    signed_pay = web_3.eth.account.sign_transaction(pay, private_key)
+
+    #send transaction
+    pay_hash = web_3.eth.sendRawTransaction(signed_pay.rawTransaction)
+    print("You rent 3 day")
+if day == "7":
+    pay = {
+        'nonce': nonce,
+        'to': host,
+        'value': web_3.toWei(7, 'ether'),
+        'gas': 2000000,
+        'gasPrice': web_3.toWei('50', 'gwei')
+    }
+
+    #sign the transaction
+    signed_pay = web_3.eth.account.sign_transaction(pay, private_key)
+
+    #send transaction
+    pay_hash = web_3.eth.sendRawTransaction(signed_pay.rawTransaction)
+    print("You rent 7 day")
